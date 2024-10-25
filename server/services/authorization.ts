8f436893592c333f49ace27f959de1a38f788d2f
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2024-10-24 20:00:40 -0400

    setup authorization function service

--- server/services/authorization.ts
+++ server/services/authorization.ts
@@ -2,36 +2,73 @@ import {generateAccessToken, generateRefreshToken, verifyAccessToken} from "@ser
 import {InsufficientPermissionsException, InvalidCredentialsException} from "../types/exceptions";
 import {Role} from "@server/types/dtos/roles";
 
-export async function verifyAuthorizationHeader(authorizationHeader) {
+export async function verifyAuthorizationHeader(authorizationHeader: string) {
     try {
-        if (!authorizationHeader || !authorizationHeader?.startsWith("Bearer ")) {
-            return null
+        if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
+            return null;
         }
 
         const token = authorizationHeader.split(" ")[1];
-
         const payload = verifyAccessToken(token);
+
+        if (!payload) {
+            throw new InvalidCredentialsException("Unable to verify token.");
+        }
+
         return payload;
     } catch (error) {
-        console.error("verifyAuthorizationHeader", error)
+        console.error("verifyAuthorizationHeader", error);
         throw new InvalidCredentialsException("Forbidden");
     }
 }
 
-export async function verifyRole(tokenPayload, requiredRole: Role) {
-    if (tokenPayload.role !== requiredRole) {
-        throw new InsufficientPermissionsException("Forbidden");
+export async function verifyAdminAuthorization(req: Request) {
+    const authorizationHeader = req.headers.get("authorization");
+
+    const tokenPayload = await verifyAuthorizationHeader(authorizationHeader);
+    if (!tokenPayload) {
+        throw new InvalidCredentialsException("Authorization failed. Invalid token.");
     }
+
+    await verifyRole(tokenPayload, Role.ADMIN);
+
     return tokenPayload
 }
 
-export async function refreshTokens(tokenPayload){
-    const {userId, email, role} = tokenPayload
-    const refreshToken = generateRefreshToken(userId, email, role)
-    const accessToken = generateAccessToken(userId, email, role)
+export async function verifyMatchingUserAuthorization(req: Request, userId: number) {
+    const authorizationHeader = req.headers.get("authorization");
+
+    const tokenPayload = await verifyAuthorizationHeader(authorizationHeader);
+    if (!tokenPayload) {
+        throw new InvalidCredentialsException("Authorization failed. Invalid token.");
+    }
+
+    await verifyMatchingUser(tokenPayload, userId)
+
+    return tokenPayload;
+}
+
+export async function verifyRole(tokenPayload: any, requiredRole: Role) {
+    if (tokenPayload.role !== requiredRole) {
+        throw new InsufficientPermissionsException("Forbidden. Insufficient permissions.");
+    }
+    return tokenPayload;
+}
+
+export async function verifyMatchingUser(tokenPayload: any, userId: number) {
+    if (Number(tokenPayload.userId) !== userId) {
+        throw new InsufficientPermissionsException("Forbidden. User ID does not match.");
+    }
+    return tokenPayload;
+}
+
+export async function refreshTokens(tokenPayload: any) {
+    const {userId, email, role} = tokenPayload;
+    const refreshToken = generateRefreshToken(userId, email, role);
+    const accessToken = generateAccessToken(userId, email, role);
 
     return {
         refreshToken,
         accessToken
-    }
+    };
 }
