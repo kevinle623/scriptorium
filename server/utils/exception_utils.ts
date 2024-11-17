0f10d1b88d5332045482f4a22355d88124bafd89
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2024-11-16 21:59:15 -0500

    exception fixing

--- server/utils/exception_utils.ts
+++ server/utils/exception_utils.ts
@@ -9,16 +9,22 @@ import {
 
 export function routeHandlerException(error: any) {
     if (error instanceof DatabaseIntegrityException) {
-        return NextResponse.json({ error: error.message }, { status: 400 });
+        // 409 Conflict: Indicates a conflict, such as duplicate or invalid data that violates database constraints
+        return NextResponse.json({ error: error.message }, { status: 409 });
     } else if (error instanceof InvalidCredentialsException) {
-        return NextResponse.json({ error: error.message }, { status: 403 });
+        // 401 Unauthorized: Credentials are invalid, but the user might authenticate successfully with valid ones
+        return NextResponse.json({ error: error.message }, { status: 401 });
     } else if (error instanceof ServiceException) {
+        // 400 Bad Request: Generic client-side error, indicating invalid input or a bad request
         return NextResponse.json({ error: error.message }, { status: 400 });
     } else if (error instanceof InsufficientPermissionsException) {
+        // 403 Forbidden: User does not have sufficient permissions to access the resource
         return NextResponse.json({ error: error.message }, { status: 403 });
     } else if (error instanceof NotFoundException) {
-        return NextResponse.json({ error: error.message }, { status: 401 });
+        // 404 Not Found: Resource not found
+        return NextResponse.json({ error: error.message }, { status: 404 });
     }
-    console.error("Unhandled Error", error)
+    console.error("Unhandled Error", error);
+    // 500 Internal Server Error: Catch-all for unexpected server errors
     return NextResponse.json({ error: "Internal server error" }, { status: 500 });
 }
