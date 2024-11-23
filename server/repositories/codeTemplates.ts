fd68279234f8379f77f976c5f7100eef22671c14
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2024-11-23 18:53:48 -0500

    bug fix

--- server/repositories/codeTemplates.ts
+++ server/repositories/codeTemplates.ts
@@ -119,7 +119,7 @@ export async function getCodeTemplatesByUserId(
                         },
                     },
                     {
-                        description: {
+                        explanation: {
                             contains: content,
                         },
                     },
